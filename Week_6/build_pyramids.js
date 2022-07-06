fe4e897c2e910a541b0221398c2768ddfe0a71b6
designerduk <superjjjdg@gmail.com>
2022-07-06 22:29:02 +0900

    Done

--- Week_6/build_pyramids.js
+++ Week_6/build_pyramids.js
@@ -1,5 +1,6 @@
-let input = 4; 
+let input = 1; 
 let level = 10; 
+
 if (input == 1) 
 for (let i = 1; i <= level; i++) {
 
@@ -7,22 +8,22 @@ for (let i = 1; i <= level; i++) {
         process.stdout.write("*");
     }
     console.log('');
-} 
+    }    
 
 else if (input == 2) {
     for (let z = 1; z <= level; z++) {
     
-        for (let k = 0; k < level-z; k++) {
+        for (let k = 0; k <= level-z; k++) {
             process.stdout.write("*");
         }
         console.log('');
-    } 
-    }
+        } 
+        }
 
 else if (input == 3) {
     for (let g = 1; g <= level; g++) {
 
-        for (let l = 0; l < level-g; l++) {
+        for (let l = 0; l <= level-g; l++) {
             process.stdout.write(" ");
         }
         for (let l = 0; l < g; l++) {
@@ -37,7 +38,7 @@ else if (input == 4) {
         for (let r = 0; r < w; r++) {
             process.stdout.write(" ");
         }
-        for (let r = 0; r < level-w; r++) {
+        for (let r = 0; r <= level-w; r++) {
             process.stdout.write("*");
         }
         console.log('');
