5dfe1cc7c4f43e081eeb9ae2bf02a5a6f390090b
designerduk <superjjjdg@gmail.com>
2022-08-04 00:05:18 +0900

    doing

--- cheungdukki/src/App.js
+++ cheungdukki/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Images from './Components/Images/Images';
 import Footer from './Components/Footer/Footer';
-
+import Navigator from './Components/Navigator/Navigator';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Images />
+      <Navigator />
+       <Routes>
+          <Route path='/' element={<Images />} />
+       </Routes>
       <Footer />
       </div> 
   )
-}
+  }
 export default App;
